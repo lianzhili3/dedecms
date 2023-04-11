81ed54fb613ad620c549d8998e4783de09450d83
lianzhli6 <lianzhili6@163.com>
2023-04-11 22:48:25 +0800

    chushihua

---
+++ data/cache/listcache/b0/e0/72/b0e0727b9c739c0fb49e8bccbac035ca.php
@@ -0,0 +1,2 @@
+<?php exit('DedeCMS Error: Request Error!');?>
+a:2:{s:4:"data";a:1:{s:2:"dd";s:2:"73";}s:7:"timeout";i:1680713115;}
---
+++ data/common.inc.php
@@ -0,0 +1,12 @@
+<?php
+//数据库连接信息
+$cfg_dbtype = 'mysql';
+$cfg_dbhost = 'localhost';
+$cfg_dbname = 'www_43_143_98_25';
+$cfg_dbuser = 'www_43_143_98_25';
+$cfg_dbpwd = 'TKaMKBjSXiWNXS8G';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
